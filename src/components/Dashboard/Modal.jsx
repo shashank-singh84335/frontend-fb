a225602ef432416aad81dd148bf6d3f059dfb27f
shashank kumar singh <shashankkumarsingh@KTSPL-M-44.local>
2024-04-03 02:54:14 +0530

    updated scope

--- src/components/Dashboard/Modal.jsx
+++ src/components/Dashboard/Modal.jsx
@@ -12,7 +12,7 @@ const Modal = ({ modalHeading , close }) => {
     Cookies.set("other_name",other_name);
     window.location.href = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${appID}&redirect_uri=${
       FACEBOOK_REDIRECT_URL
-    }/dashboard/&scope=pages_show_list, pages_read_user_content, pages_manage_engagement, pages_manage_posts, pages_read_engagement, publish_video&response_type=token&state=${appID},${appSecret}`;
+    }/dashboard/&scope=pages_show_list, pages_read_user_content, pages_manage_engagement, pages_manage_posts, pages_read_engagement, publish_video,read_insights&response_type=token&state=${appID},${appSecret}`;
 }
   return (
     <div
